97a0911930c9eb3eac75d7c6c8d83aa4b2ccb7d9
Douglas Augusto <douglas.full.ti@gmail.com>
2024-09-09 20:35:41 -0300

    teste

--- src/App/Main.java
+++ src/App/Main.java
@@ -4,39 +4,63 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Account;
+import entities.Person;
 
 public class Main {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        double media, sum, alturaMenores, percentualMenores;
+        int idade;
+        String names;
 
-        double sum, media;
-
-        System.out.print("Quantos numeros você vai digitar? ");
+        System.out.print("Quantas pessoas serao digitadas? ");
         int n = sc.nextInt();
 
-        double[] vect = new double[n];
+        Person[] vect = new Person[n];
 
         for (int i = 0; i < vect.length; i++) {
+            System.out.printf("Dados da %da pessoa:%n", i + 1);
+            System.out.print("Nome: ");
+            String name = sc.nextLine();
             sc.nextLine();
-            System.out.print("Digite um numero: ");
-            vect[i] = sc.nextDouble();
+            System.out.print("Idade: ");
+            int age = sc.nextInt();
+
+            System.out.print("Altura: ");
+            double height = sc.nextDouble();
+
+            vect[i] = new Person(name, age, height);
         }
+        sc.nextLine();
+
         sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = sum + vect[i];
+            sum = sum + vect[i].getHeight();
+
         }
         media = sum / n;
-
-        System.out.println("VALORES = ");
+        idade = 0;
+        names = "";
+        alturaMenores = 0;
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("%.1f " , vect[i]);
+            if (idade < 16) {
+                alturaMenores++;
+            }
         }
-        System.out.printf("\nSOMA = %.2f\n", sum);
+        percentualMenores = ((double) alturaMenores / n) * 100.0;
 
-        System.out.printf("MEDIA = %.2f", media);
+        for (int i = 0; i < n; i++) {
+            idade = idade + vect[i].getAge();
+            if (alturaMenores < 16) {
+                System.out.printf("%s\n", names);
+            }
+        }
+
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
+        System.out.printf("Altura média: %.2f", media);
 
         sc.close();
     }
