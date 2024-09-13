c8bfbc21aaccd412772d134aa4c5555b90aa9012
Douglas Augusto <douglas.full.ti@gmail.com>
2024-09-13 19:57:32 -0300

    cod. final exercico 03 fixação vetores

--- src/App/Main.java
+++ src/App/Main.java
@@ -32,36 +32,34 @@ public class Main {
 
             vect[i] = new Person(name, age, height);
         }
-       
-
         sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum = sum + vect[i].getHeight();
-
-        }
-        media = sum / n;
         idade = 0;
         names = "";
         alturaMenores = 0;
 
         for (int i = 0; i < n; i++) {
+            sum = sum + vect[i].getHeight();
+
+        }
+        for (int i = 0; i < vect.length; i++) {
             if (vect[i].getAge() < 16) {
                 alturaMenores++;
-                names = names + vect[i].getName();
+
             }
         }
-        System.err.println("");
+
+        media = sum / n;
 
         percentualMenores = ((double) alturaMenores / n) * 100.0;
 
-        
         System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
-        System.out.printf("Altura média: %.2f", media);
-        
+        System.out.printf("Altura média: %.2f\n", media);
+
         for (int i = 0; i < n; i++) {
-            if (alturaMenores <= 16) {
-                System.out.println(names);
+            if (vect[i].getAge() < 16) {
+                System.out.printf("%s \n", vect[i].getName());
             }
+
         }
 
         sc.close();
