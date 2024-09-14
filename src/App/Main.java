484b95804e53f7a7478fed35bb4d22ac78f2f4dd
Douglas Augusto <douglas.full.ti@gmail.com>
2024-09-14 20:50:21 -0300

    alteração exercício de vetores com numeros pares

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,19 +3,33 @@ package App;
 import java.util.Locale;
 import java.util.Scanner;
 
-
-
 public class Main {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int n = 0;
+        int n, qntpares;
 
-        Number[] numbers = new Number[n];
-        
+        System.out.print("Quantos numeros voce vai digitar? ");
 
+        n = sc.nextInt();
 
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Digite um numero: ");
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println("\nNUMEROS PARES:");
+        qntpares = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.printf("%d ", numbers[i]);
+                qntpares++;
+            }
+        }
+        System.out.printf("\n\nQUANTIDADE DE PARES = %d\n " , qntpares);
 
         sc.close();
     }
